fef5fd1f1a2cd3de459eaa7afdf57abdfdbdd585
Gonza-Gil <gonzalogil123@gmail.com>
2022-02-24 22:02:20 -0300

    Comentarios

--- mapit.py
+++ mapit.py
@@ -1,3 +1,5 @@
+# Aplicación que busca en google maps una dirección ingresada por teclado o copiada del portapapeles
+
 import webbrowser, pyperclip
 
 address = input("Ingrese la dirección que desea buscar (si quiere importar desde el porta papeles, presione ENTER): ")
